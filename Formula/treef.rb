ae65729d6cf0d028c0494c703445460839b8a212
Cheng® <31495069+mark22013333@users.noreply.github.com>
2025-07-17 00:55:08 +0800

    更新 treef 到 v1.0.2

--- Formula/treef.rb
+++ Formula/treef.rb
@@ -1,8 +1,8 @@
 class Treef < Formula
     desc "高顏值 CLI 目錄樹狀結構顯示工具（Fancy tree viewer for directories）"
     homepage "https://github.com/mark22013333/treef"
-    url "https://github.com/mark22013333/treef/archive/refs/tags/v1.0.1.tar.gz"
-    sha256 "c7788d2f4ef4d95a0c30af9d1130c6727d12f95e394d67a107428f64b1656856"
+    url "https://github.com/mark22013333/treef/archive/refs/tags/v1.0.2.tar.gz"
+    sha256 "352eb111b3cef7af6e6ff8a239ce7a26bfe88b4cdad5e8ee70182f1199109909"
     license "MIT"
   
     def install
